fafeaa4a317d3a2e0b7e2df1d1c1fadf18d0c5ab
BoyLondon <fucshu@163.com>
2018-03-28 15:52:01 +0800

    add files

---
+++ controllers/mainCtrl.js
@@ -0,0 +1,4 @@
+angular.module("mainM")
+    .controller("mainCtrl", function($scope){
+
+    });
---
+++ controllers/summaryCtrl.js
@@ -0,0 +1,5 @@
+angular.module("summary",[])
+    .constant("summaryurl", "http://localhost:5800/summary")
+    .controller("summaryCtrl", function($scope, summaryurl){ 
+        
+    })
