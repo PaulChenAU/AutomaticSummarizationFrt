a22c2212a16992fd5c7e95d9f939d64238ca25ee
BoyLondon <fucshu@163.com>
2018-03-30 00:48:51 +0800

    update user component

--- components/user/user.js
+++ components/user/user.js
@@ -1,4 +1,6 @@
 angular.module("user",[])
+    .constant("default_avatar_url", "http://localhost:5800/avatar/")
+    .constant("default_avatar_size", 0)
     .factory("user", function(){
         return {
 
@@ -8,8 +10,14 @@ angular.module("user",[])
         return {
             restrict: "E",
             templateUrl: "components/user/mainUser.html",
-            controller: function($scope){
-
+            controller: function($scope,default_avatar_url,default_avatar_size){
+                $scope.$on("loginM", function(event, msg){
+                    $scope.user = msg;
+                    $scope.login = msg["loginStatus"];
+                    if (!$scope.user.avatar_url || $scope.user.avatar_url.length == 0){
+                        $scope.user.avatar_url = default_avatar_url + $scope.user.username + "/" + default_avatar_size;
+                    }
+                })
             }
         }
     });
